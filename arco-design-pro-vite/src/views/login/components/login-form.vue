ac5e1d00dff5bdf36e4b5d531ec8b0cf5711baee
白开水浇花 <fengqiyue@gmail.com>
2025-04-05 00:50:40 +0800

    fix: 修复 @arco-design/web-vue 导入路径，合并导入语句

--- arco-design-pro-vite/src/views/login/components/login-form.vue
+++ arco-design-pro-vite/src/views/login/components/login-form.vue
@@ -53,8 +53,7 @@
 <script lang="ts" setup>
   import { ref, reactive } from 'vue';
   import { useRouter } from 'vue-router';
-  import { Message } from '@arco-design/web-vue';
-  import { ValidatedError } from '@arco-design/web-vue/es/form/interface';
+  import { Message, ValidatedError } from '@arco-design/web-vue';
   import { useI18n } from 'vue-i18n';
   import { useStorage } from '@vueuse/core';
   import { useUserStore } from '@/store';
