4acd0f0946ff2f3cac614abc888cdb7be3b03f1a
白开水浇花 <fengqiyue@gmail.com>
2025-04-16 21:00:07 +0800

    feat: 登录页跟随主题

--- arco-design-pro-vite/src/views/login/index.vue
+++ arco-design-pro-vite/src/views/login/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page-container">
+  <div class="login-container">
     <div class="logo">
       <div class="logo-text">{{ appStore?.app_name }}</div>
     </div>
@@ -16,14 +16,26 @@
 
 <script lang="ts" setup>
   import { useAppStore } from '@/store';
+  import { useDark } from '@vueuse/core';
   import Footer from '@/components/footer/index.vue';
   import LoginForm from './components/login-form.vue';
 
   const appStore = useAppStore();
+
+  useDark({
+    selector: 'body',
+    attribute: 'arco-theme',
+    valueDark: 'dark',
+    valueLight: 'light',
+    storageKey: 'arco-theme',
+    onChanged(dark: boolean) {
+      appStore.toggleTheme(dark);
+    },
+  });
 </script>
 
 <style lang="less" scoped>
-  .page-container {
+  .login-container {
     background-image: url(assets/images/login-bg.png);
     background-position: 50%;
     background-repeat: no-repeat;
@@ -42,7 +54,7 @@
       &-inner {
         @apply w-full max-w-sm lg:max-w-md;
         padding: 32px;
-        background-color: #ffffff;
+        background: var(--color-fill-1);
         border-radius: 12px;
         overflow: hidden;
       }
@@ -71,6 +83,12 @@
       color: var(--color-text-1);
       font-size: 20px;
       font-family: '钉钉进步体 Regular', sans-serif;
+    }
+  }
+
+  body[arco-theme='dark'] {
+    .login-container {
+      background: var(--color-fill-2);
     }
   }
 </style>
